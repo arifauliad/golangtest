a36a491a10d86c7b6cc300110100f0c52f49050a
arifauliad <48560951+arifauliad@users.noreply.github.com>
2019-03-20 00:22:28 +0700

    Create sorting2.go

---
+++ sorting2.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("masukan angka")
+	var a [6]int
+	var tempvalue int
+	for i := 0; i < 6; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	fmt.Println(a[0], a[1], a[2], a[3], a[4], a[5])
+
+	for i := 1; i < 6; i++ {
+		for j := i; j > 0; j-- {
+			if a[j] < a[(j-1)] {
+				tempvalue = a[(j - 1)]
+				a[(j - 1)] = a[j]
+				a[j] = tempvalue
+			}
+			printgaris(a[:])
+		}
+	}
+}
+
+func printgaris(a []int) {
+
+	var max, comparator int
+	max = a[0]
+
+	for i := 1; i < 6; i++ {
+		if max < a[i] {
+			max = a[i]
+		}
+	}
+
+	for i := max; i > 0; i-- {
+		comparator = i - 1
+		for j := 0; j < 6; j++ {
+			if comparator < a[j] {
+				fmt.Print("| ")
+			} else {
+				fmt.Print("  ")
+			}
+		}
+		fmt.Println("")
+	}
+	fmt.Println(a[0], a[1], a[2], a[3], a[4], a[5])
+}
